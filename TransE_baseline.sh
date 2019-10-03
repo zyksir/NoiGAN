2cbb3a11c3800f191a4bbdcc45374be57f037612
yikai Zhu <yikaizhu@yikaideMacBook-Pro.local>
2019-10-03 09:28:54 +0800

    delete something

--- TransE_baseline.sh
+++
@@ -1,6 +0,0 @@
-# bash run.sh train TransE NELL27k 1 base_clf 512 1024 500 12.0 0.5 0.0001 100000 8 --method clf -init ./models/TransE_NELL27k_baseline
-#bash run.sh train TransE NELL27k 1 base_clf_hard 512 1024 500 12.0 0.5 0.0001 100000 8 --method clf -init ./models/TransE_NELL27k_baseline --num 1000
-#bash run.sh train TransE YAGO3-10 1 base_clf_soft 512 1024 500 12.0 0.5 0.0001 100000 8 --method clf -init ./models/TransE_YAGO3-10_baseline --num 1
-#bash run.sh train TransE YAGO3-10 1 base_clf_hard 512 1024 500 12.0 0.5 0.0001 100000 8 --method clf -init ./models/TransE_YAGO3-10_baseline --num 1000
-bash run.sh train TransE YAGO3-10 1 fake10 512 1024 500 12.0 0.5 0.0001 100000 8 --fake 10
-bash run.sh train TransE NELL27k 1 fake10 512 1024 500 12.0 0.5 0.0001 100000 8 --fake 10
